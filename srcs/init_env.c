604229c8170dd8ba8c5738fc4482975b402e2b4d
Kalilo <khansman@e5r8p1.jnb.42.fr>
2016-08-23 08:35:03 +0200

    Fixed norme

--- srcs/init_env.c
+++ srcs/init_env.c
@@ -18,9 +18,9 @@
 
 void	init_env(t_env *env)
 {
-	if (!(env->core = malloc(CORESIZE * sizeof(int))) ||
+	if (!(env->memory = malloc(CORESIZE * sizeof(int))) ||
 			!(env->processes = malloc((MAX_PROCESS + 2) * sizeof(t_process))))
 		error_quit(0);
-	ft_bzero(env->core, (CORESIZE * sizeof(int)));
+	ft_bzero(env->memory, (CORESIZE * sizeof(int)));
 	ft_bzero(env->processes, ((MAX_PROCESS + 2) * SIZEOF(t_process)));
 }
