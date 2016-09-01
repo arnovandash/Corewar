0fe471409af8b4da003b25756f52b088c57ddc1b
Robert JONES <rojones@e5r10p5.jnb.42.fr>
2016-09-01 09:23:29 +0200

    ft_fork added to function array

--- srcs/init_functions.c
+++ srcs/init_functions.c
@@ -6,7 +6,7 @@
 /*   By: rojones <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/08/23 16:59:18 by rojones           #+#    #+#             */
-/*   Updated: 2016/08/31 07:48:30 by rojones          ###   ########.fr       */
+/*   Updated: 2016/09/01 09:23:09 by rojones          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int	(*function[17])(struct s_env *env, t_arg_code arg_code, t_process *process) 
 	0,//ft_zjump,
 	0,//ft_load_index,
 	0,//ft_store_index,
-	0,//ft_fork,
+	ft_fork,
 	0,//ft_long_load,
 	0,//ft_long_load_index,
 	0,//ft_long_fork,
